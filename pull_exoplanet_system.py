51c289b9c44aa8f6e84ad6e1fb72aa32e310a4d1
Duncan Forgan <dhf@roe.ac.uk>
2014-06-26 10:49:09 +0100

    Begun bringing methods into `pull_exoplanet_system.py`
    Bringing in wavebender will be messy - maybe rethink this
    
    `single_system_note.py` no longer has any direct calls to wavebender
    Lots of tidying to do!

--- pull_exoplanet_system.py
+++ pull_exoplanet_system.py
@@ -1,10 +1,19 @@
 import xml.etree.ElementTree as ET
+from wavebender import damped_wave, compute_samples, write_wavefile
+
+
+jup2EarthMass = 317.83
+jup2EarthRadii = 11.22
+year = 365.24
+
+
+# Define the limits of human hearing!
+frequency_min = 50 # giant planets will reach the low frequencies
+frequency_max = 1.0e4 # low mass planets will reach these frequencies
 
 def pull_exoplanet_system(xmlfile):
     
-    jup2EarthMass = 317.83
-    jup2EarthRadii = 11.22
-    year = 365.24
+
     
     # Read in XML file
 
@@ -58,6 +67,79 @@ def pull_exoplanet_system(xmlfile):
             if(masses[-1]>300):
                 radii.append(11.2)
             else:
-                radii.append(masses[-1]**0.666)
+                radii.append(masses[-1]**0.666) # TODO - check this is the correct prescription!
                   
     return periods, masses, radii
+
+def generate_notes_periods_radii(periods,radii, fileTime, wavefile, frameRate=44100, middleC = 1000.0):
+    '''Generates a .wav file using the periods and radii of the exoplanet dataset'''
+    
+    pitches = []
+    
+    for i in range(len(radii)):
+        pitches.append(middleC/radii[i])
+    
+        # Keep the notes within human hearing!
+        if(pitches[-1]<frequency_min): pitches[-1]=frequency_min
+        if(pitches[-1]>frequency_max): pitches[-1]=frequency_max
+    
+
+        # Notes per second - longer periods mean longer durations between notes
+        # This normalisation means that a period of 1 year = one second between notes
+
+    repeatrates = []
+    for i in range(len(periods)):
+        repeatrates.append(frameRate*periods[i])
+
+    # Create lists of iterator objects for each channel
+    # Terrestrial planets will be in channel 1
+    # Giant planets will be in channel 2
+
+    notes1 = []
+    notes2 = []
+
+    # If there's only one planet in the system, then put it on both channels
+    if(len(periods)==1):
+        notes1.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i]))
+        notes2.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i]))
+
+    else:
+        for i in range(len(periods)):
+            print "Creating note ",i, "radius: ",radii[i]," pitch: ",pitches[i]
+    
+            if(radii[i]>3.0):
+                notes2.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i])) 
+            else:
+                notes1.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i])) 
+        
+        
+    create_wavefile(notes1,notes2,fileTime,wavefile)
+    
+    
+def generate_notes_periods_only(periods, wavefile, frameRate=44100, middleC = 1000.0):
+    '''Generates a .wav file using the orbital periods alone from the exoplanet dataset'''
+    
+    
+def create_wavefile(notes1,notes2, fileTime, wavefile, frameRate = 44100):
+    '''Generates a wavefile given notes for channels 1 and 2'''
+    
+    # Build each channel
+
+    channel1 = (notes1[:])   
+    channel2 = (notes2[:])
+    channels = (channel1,channel2)
+
+    print "Channels constructed"
+    print "Channels constructed"
+
+    # Create a set of samples from these channels
+
+    samples = compute_samples(channels, nsamples = fileTime*frameRate)
+
+    print "Samples made: writing to wavefile ",wavefile
+    print "This can take a while, please be patient!"
+
+    # Writes the output to the .wav file
+    write_wavefile(wavefile, samples)
+
+    print "File ",wavefile, " written"
--- single_system_note.py
+++ single_system_note.py
@@ -2,7 +2,7 @@
 # Creates a wav file with all planets in a system creating notes
 # Parses an XML file to find masses and periods
 
-from wavebender import damped_wave, compute_samples, write_wavefile
+
 import pull_exoplanet_system as exo
 import glob
 from os import path
@@ -15,10 +15,7 @@ middleC = 1000.0
 
 catalogue_dir = "/Users/dhf/programs/open_exoplanet_catalogue/systems/"
 
-# Define the limits of human hearing!
 
-frequency_min = 50 # giant planets will reach the low frequencies
-frequency_max = 1.0e4 # low mass planets will reach these frequencies
 
 xmlfile = []
 
@@ -51,68 +48,72 @@ filename = catalogue_dir+xmlfile
 # Pull the data from the XML file
 periods,masses,radii = exo.pull_exoplanet_system(filename)
 
-# TODO - Figure out what to do when exoplanet file incomplete (no radii)
-
-
 # How long will the file last in seconds?
 fileTime = 100.0
 
-# Use radii to generate a pitch
-# Larger planets have a lower pitch
+exo.generate_notes_periods_radii(periods,radii, fileTime, wavfile, frameRate=44100, middleC = 1000.0)
 
-pitches = []
-for i in range(len(radii)):
-    pitches.append(middleC/radii[i])
-    
-    # Keep the notes within human hearing!
-    if(pitches[-1]<frequency_min): pitches[-1]=frequency_min
-    if(pitches[-1]>frequency_max): pitches[-1]=frequency_max
-    
-
-# Notes per second - longer periods mean longer durations between notes
-# This normalisation means that a period of 1 year = one second between notes
-
-repeatrates = []
-for i in range(len(periods)):
-    repeatrates.append(frameRate*periods[i])
-
-# Create lists of iterator objects for each channel
-# Terrestrial planets will be in channel 1
-# Giant planets will be in channel 2
-
-notes1 = []
-notes2 = []
-
-# If there's only one planet in the system, then put it on both channels
-if(len(periods)==1):
-    notes1.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i]))
-    notes2.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i]))
-
-else:
-    for i in range(len(periods)):
-        print "Creating note ",i, "radius: ",radii[i]," pitch: ",pitches[i]
-    
-        if(radii[i]>3.0):
-            notes2.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i])) 
-        else:
-            notes1.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i])) 
-        
-# Build each channel
-
-channel1 = (notes1[:])   
-channel2 = (notes2[:])
-channels = (channel1,channel2)
-
-print "Channels constructed"
-
-# Create a set of samples from these channels
-
-samples = compute_samples(channels, nsamples = fileTime*frameRate)
-
-print "Samples made: writing to wavefile ",wavfile
-print "This can take a while, please be patient!"
-
-# Writes the output to the .wav file
-write_wavefile(wavfile, samples)
-
-print "File ",wavfile, " written"
+# TODO - Figure out what to do when exoplanet file incomplete (no radii)
+#
+#
+#
+#
+## Use radii to generate a pitch
+## Larger planets have a lower pitch
+#
+#pitches = []
+#for i in range(len(radii)):
+#    pitches.append(middleC/radii[i])
+#    
+#    # Keep the notes within human hearing!
+#    if(pitches[-1]<frequency_min): pitches[-1]=frequency_min
+#    if(pitches[-1]>frequency_max): pitches[-1]=frequency_max
+#    
+#
+## Notes per second - longer periods mean longer durations between notes
+## This normalisation means that a period of 1 year = one second between notes
+#
+#repeatrates = []
+#for i in range(len(periods)):
+#    repeatrates.append(frameRate*periods[i])
+#
+## Create lists of iterator objects for each channel
+## Terrestrial planets will be in channel 1
+## Giant planets will be in channel 2
+#
+#notes1 = []
+#notes2 = []
+#
+## If there's only one planet in the system, then put it on both channels
+#if(len(periods)==1):
+#    notes1.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i]))
+#    notes2.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i]))
+#
+#else:
+#    for i in range(len(periods)):
+#        print "Creating note ",i, "radius: ",radii[i]," pitch: ",pitches[i]
+#    
+#        if(radii[i]>3.0):
+#            notes2.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i])) 
+#        else:
+#            notes1.append(damped_wave(frequency=pitches[i], amplitude=radii[i]*0.01, framerate = frameRate, length= repeatrates[i])) 
+#        
+## Build each channel
+#
+#channel1 = (notes1[:])   
+#channel2 = (notes2[:])
+#channels = (channel1,channel2)
+#
+#print "Channels constructed"
+#
+## Create a set of samples from these channels
+#
+#samples = compute_samples(channels, nsamples = fileTime*frameRate)
+#
+#print "Samples made: writing to wavefile ",wavfile
+#print "This can take a while, please be patient!"
+#
+## Writes the output to the .wav file
+#write_wavefile(wavfile, samples)
+#
+#print "File ",wavfile, " written"
